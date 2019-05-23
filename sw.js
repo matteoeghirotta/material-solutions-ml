4f954c45d768f2b0bdd2184368ced0e9e173b2a8
Matteo Ricci <matteoeghirotta@gmail.com>
2019-05-23 14:54:19 +0200

    fix sw.js

--- sw.js
+++ sw.js
@@ -1,20 +1,30 @@
-importScripts('/cache-polyfill.js');
-
 self.addEventListener('install', function(e) {
- e.waitUntil(
-   caches.open('material-solutions').then(function(cache) {
-     return cache.addAll([
-       '/',
-       '/index.html',
-       '/main.css',
-       '/webgl-bundle.min.js',
-       '/smiles-drawer.min.js'
-     ]);
-   })
- );
+    e.waitUntil(
+	caches.open('material-solutions').then(function(cache) {
+	    return cache.addAll([
+		'/',
+		'/index.html',
+		'/main.css',
+		'/webgl-bundle.min.js',
+		'/smiles-drawer.min.js',
+		'/a-npd.xyz'
+	    ]);
+	})
+    );
 });
 
-self.addEventListener('fetch', function(event) {
- console.log(event.request.url);
+self.addEventListener('activate', event => {
+    event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', event => {
+    console.log(event.request.url);
+    event.respondWith(
+	caches.open(cacheName)
+	    .then(cache => cache.match(event.request, {ignoreSearch: true}))
+	    .then(response => {
+		return response || fetch(event.request);
+	    })
+    );
 });
 
